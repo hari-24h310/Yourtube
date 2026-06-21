79e406e411bfe06d64af4bd5bf1947972fe8fc20
hari-24h310 <24h310@amjaincollege.edu.in>
2026-06-21 23:32:25 +0530

    fix gmail smtp port 587

--- utils/otp.js
+++ utils/otp.js
@@ -5,7 +5,9 @@ import crypto from "crypto";
 import Otp from "../Models/Otp.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASSWORD,
